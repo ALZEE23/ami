4f2536cc54176581a2c266b21ccfc922a2fc9987
Dyzean <ashtrath@pm.me>
2024-06-08 11:15:30 +0700

    feat: improve login page

--- resources/js/Layouts/AuthLayout.tsx
+++ resources/js/Layouts/AuthLayout.tsx
@@ -1,10 +1,30 @@
-import { PropsWithChildren } from "react";
+import { Skeleton } from "@/Components/ui/skeleton";
+import { PropsWithChildren, useState } from "react";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
+  const [loaded, setLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setLoaded(true);
+  };
+
   return (
-    <main className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <main className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <aside className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900"></div>
+        {!loaded && <Skeleton />}
+        <img
+          src={"https://www.tripsavvy.com/thmb/Qid6z3y2eiQra_FUtoMA64jK878=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/MountBatur4WEB-a3e94537b1794ce982c35ee144ec939d.jpg"}
+          onLoad={handleImageLoad}
+          className={`absolute inset-0 size-full ${loaded ? "block" : "hidden"}`}
+        />
+        <div className="bg-zinc-900/50 absolute inset-0 z-10"></div>
+        <div className="relative z-20 flex items-center text-lg font-semibold">Aplikasi Manajemen Inovasi</div>
+        <div className="relative z-20 mt-auto">
+          <blockquote className="flex flex-col gap-2">
+            <p className="text-lg">"Gunung Batur"</p>
+            <footer className="text-sm">Bali, Kab. Bangli</footer>
+          </blockquote>
+        </div>
       </aside>
       <section className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center gap-y-6 sm:w-[350px]">
--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,26 +1,8 @@
 import LoginForm from '@/Components/LoginForm';
 import AuthLayout from '@/Layouts/AuthLayout';
-import { Head, useForm } from '@inertiajs/react';
-import { FormEventHandler, useEffect } from 'react';
+import { Head } from '@inertiajs/react';
 
-export default function Login({ status, canResetPassword }: { status?: string, canResetPassword: boolean }) {
-  const { data, setData, post, processing, errors, reset } = useForm({
-    email: '',
-    password: '',
-    remember: false,
-  });
-
-  useEffect(() => {
-    return () => {
-      reset('password');
-    };
-  }, []);
-
-  const submit: FormEventHandler = (e) => {
-    e.preventDefault();
-
-    post(route('login'));
-  };
+export default function Login() {
 
   return (
     <>
