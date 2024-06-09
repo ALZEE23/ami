79747227219a0b7692eab73ade8f628e6f56e21a
Dyzean <ashtrath@pm.me>
2024-06-10 03:16:12 +0700

    style: lint entire codebase

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,28 +1,33 @@
-import ReactDOMServer from 'react-dom/server';
-import { createInertiaApp } from '@inertiajs/react';
-import createServer from '@inertiajs/react/server';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { route } from '../../vendor/tightenco/ziggy';
-import { RouteName } from 'ziggy-js';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import ReactDOMServer from "react-dom/server";
+import { createInertiaApp } from "@inertiajs/react";
+import createServer from "@inertiajs/react/server";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { route } from "../../vendor/tightenco/ziggy";
+import { type RouteName } from "ziggy-js";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = (import.meta.env.VITE_APP_NAME as string) || "Laravel";
 
 createServer((page) =>
-    createInertiaApp({
-        page,
-        render: ReactDOMServer.renderToString,
-        title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
-        setup: ({ App, props }) => {
-            global.route<RouteName> = (name, params, absolute) =>
-                route(name, params as any, absolute, {
-                    // @ts-expect-error
-                    ...page.props.ziggy,
-                    // @ts-expect-error
-                    location: new URL(page.props.ziggy.location),
-                });
+  createInertiaApp({
+    page,
+    render: ReactDOMServer.renderToString,
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+      resolvePageComponent(
+        `./Pages/${name}.tsx`,
+        import.meta.glob("./Pages/**/*.tsx"),
+      ),
+    setup: ({ App, props }) => {
+      global.route<RouteName> = (name, params, absolute) =>
+        route(name, params, absolute, {
+          // @ts-expect-error
+          ...page.props.ziggy,
+          // @ts-expect-error
+          location: new URL(page.props.ziggy.location),
+        });
 
-            return <App {...props} />;
-        },
-    })
+      return <App {...props} />;
+    },
+  }),
 );
