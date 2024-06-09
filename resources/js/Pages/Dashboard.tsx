fa8b96ccd061d9201ad53cf37542b7dbbc20de2d
Dyzean <ashtrath@pm.me>
2024-06-10 05:46:51 +0700

    feat: update dashboard

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -6,17 +6,19 @@ import { type PageProps } from "@/types";
 
 export default function Dashboard({ auth }: PageProps) {
   return (
-    <DashboardLayout>
-      <ContentLayout title="Dashboard" user={auth.user}>
-        <Head title="Dashboard" />
-        <div className="py-12">
-          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-              <div className="p-6 text-gray-900">You're logged in!</div>
+    <>
+      <Head title="Dashboard" />
+      <DashboardLayout>
+        <ContentLayout title="Dashboard" user={auth.user}>
+          <div className="border bg-card text-card-foreground shadow rounded-lg border-none">
+            <div className="p-6">
+              <div className="flex justify-center items-center min-h-[calc(100vh-56px-56px-64px-48px)]">
+                You're Logged In!
+              </div>
             </div>
           </div>
-        </div>
-      </ContentLayout>
-    </DashboardLayout>
+        </ContentLayout>
+      </DashboardLayout>
+    </>
   );
 }
