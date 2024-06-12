acee89d5835d7ee5a1017ba7a7c0f7f847726d99
Dyzean <ashtrath@pm.me>
2024-06-12 17:26:43 +0700

    feat: add SidebarMobile component

--- resources/js/Components/Layout/NavBar.tsx
+++ resources/js/Components/Layout/NavBar.tsx
@@ -1,4 +1,6 @@
 import { type User } from "@/types"
+
+import { SidebarMobile } from "../Sidebar/SidebarMobile"
 import UserNav from "./UserNav"
 
 type NavBarProps = {
@@ -11,6 +13,7 @@ export default function NavBar({ title, user }: NavBarProps) {
     <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 sm:mx-8 flex h-14 items-center">
         <div className="flex items-center gap-x-4 lg:gap-x-0">
+          <SidebarMobile />
           <h1 className="font-bold">{title}</h1>
         </div>
         <div className="flex flex-1 items-center gap-x-2 justify-end">
